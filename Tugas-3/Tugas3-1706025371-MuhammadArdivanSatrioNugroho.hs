da61b71fcf2c26c1f71e99690ad1021a620156c0
xb4dc0d3 <johnbakeryholland@gmail.com>
2020-10-04 22:51:22 +0700

    Finish tugas-3 pemfung

--- Tugas-3/Tugas3-1706025371-MuhammadArdivanSatrioNugroho.hs
+++ Tugas-3/Tugas3-1706025371-MuhammadArdivanSatrioNugroho.hs
@@ -1,4 +1,7 @@
+import Text.Printf (printf)
+
 --- Soal no.1 ---
+
 myCurry :: ((a,b)->c)->(a->b->c)
 myCurry f x y = f (x,y)
 
@@ -6,42 +9,50 @@ myUncurry :: (a->b->c)-> (a,b)->c
 myUncurry f (x,y) = f x y
 
 --- Soal no,2 ---
--- fib_helper :: Integer -> Integer
--- fib_helper num 
---         | num == 0 = 0
---         | num == 1 = 1
---         | otherwise = fib_helper(num-1) + fib_helper(num-2)
--- fibonacci :: Integer -> [(Integer)]
--- fibonacci num = map fib_helper [0..num]
+
+--- Helper function to add the next val ---
 fib_helper :: [Integer]
 fib_helper = 0 : next
     where next = 1 : zipWith (+) fib_helper next
 
+--- Get Fibonacci List ---
 fibonacci :: Int -> [Integer]
-fibonacci = flip take fib_helper
+fibonacci n = take (n+1) fib_list
+  where fib_list = 0:1:(zipWith (+) fib_list (tail fib_helper))
 
 --- Soal no.3 ---
 multiply_helper :: Integer -> Integer -> Integer
+--- Addition method ---
 multiply_helper x y 
             | y == 0 = 0
             | y == 1 = x
-            | otherwise = x + (multiply_helper x (y-1)) ---addition method---
+            | otherwise = x + (multiply_helper x (y-1))
 
 power :: Integer -> Integer -> Integer
-power x y
-        | y == 0 = 1
-        | y == 1 = x
-        | otherwise = (multiply_helper x (power x (y-1)))
+power a b
+        | a == 0 = 1
+        | b == 1 = a
+        | otherwise = (multiply_helper a (power a (b-1)))
 
 --- Soal no.4 ---
+
 sumEven :: [(Integer)] -> Integer
+--- Filter the even number and sum them all ---
 sumEven listInteger = sum (filter even listInteger)
 
 --- Soal no.5 ---
+
+--- Decimal precision helper ---
+trunc_dec :: Double -> Int -> Double
+trunc_dec result dec_prec = (fromIntegral (round (result * t))) / t
+    where t = 10^dec_prec
+
+--- Asumsi: boleh menggunakan rekursif biasa ---
 eq_invest  ::Double->Double->Double->Double
 eq_invest nominal rate duration 
                         | duration == 0 = nominal
                         | otherwise = nominal + eq_invest (nominal * (1+rate/100)) rate (duration-1)
 
+--- Asumsi: decimal precision 1 after comma ---
 invest :: Double -> Double -> Double -> Double
-invest nom rate dur = (eq_invest nom rate dur) - nom
+invest nom rate dur = trunc_dec ((eq_invest nom rate dur) - nom) 1
