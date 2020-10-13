3be4ea9c395d9236c492367390271ae18c0309f6
xb4dc0d3 <johnbakeryholland@gmail.com>
2020-10-13 19:37:30 +0700

    Add Tugas-4 Pemfung v2

--- Tugas-4/Tugas4-1706025371-MuhammadArdivanSatrioNugroho.hs
+++ Tugas-4/Tugas4-1706025371-MuhammadArdivanSatrioNugroho.hs
@@ -5,41 +5,54 @@ import Data.Char as Char
 length_by_foldl :: [a] -> Integer
 length_by_foldl xs = foldl (\acc _ -> acc + 1) 0 xs
 
-length_by_foldr ::  [a] -> Integer
+-- length_by_foldr ::  [a] -> Integer
 --- const x is a unary function which evaluates to x for all inputs.---
-length_by_foldr xs = foldr (const (1+)) 0 xs
+-- length_by_foldr xs = foldr (const (1+)) 0 xs
 
 --- using map ---
-length_by_map :: [a] -> Integer
-length_by_map = sum . (map (\_ -> 1))
+length' :: [a] -> Integer
+length' xs = foldr (+) 0 (map (\_ -> 1) xs)
 
 --- Soal nomor 2 ---
-total func n = sum (map func [1..n])
+-- greaterOne n = n > 1
+-- addOne n = n + 1
+-- addUp  ns  =  filter  greaterOne  (map  addOne  ns)
+addUp' :: (Num b, Ord b) => [b] -> [b]
+addUp' ns =  map (\x -> x+1) ((filter (\x -> x>0) ns))
 
 --- Soal nomor 3 ---
-sumOfSquares = 0
+--- a. Map and Fold ---
+sumOfSquares_a :: Integer -> Integer
+sumOfSquares_a n = foldl (+) 0 (map (\x-> x^2) [1.. n])
+
+--- b. Fold and List Comprehension ---
+sumOfSquares_b :: Integer -> Integer
+sumOfSquares_b n = foldl (+) 0 ([x^2 | x <- [1..n]])
+
+--- c. Perbedaan dari kedua pendekatan ---
+
 
 --- Soal nomor 4 ---
 multipleOf5 :: Integral a => [a] -> Integer
---- reuse length_by_foldl ---
-multipleOf5 list = length_by_foldl (map (\x -> x) (filter (\x -> x `mod` 5 == 0) list))
+--- Reuse length' expression ---
+multipleOf5 list = length' (map (\x -> x) (filter (\x -> x `mod` 5 == 0) list))
 
 
 --- Soal nomor 5 ---
--- total :: (Int -> Int) -> (Int -> Int)
-
+total func n = sum (map func [1..n])
 
 --- Soal nomor 6 ---
 reverse' :: Foldable t => t a -> [a]
 reverse' xs = foldr (\x acc -> acc ++ [x]) [] xs
 
+--- Soal nomor 7 (PDF) ---
 
 --- Soal nomor 8 ---
 pythaTriple :: [(Integer, Integer, Integer)]
 pythaTriple = [(x,y,z) | z<- [5..], y<- [z, (z-1) .. 1], x<- [y, (y-1)..1], x*x + y*y == z*z]
 
 
---- Soal nomor 10 ---
+--- Soal nomor 9 (PDF) ---
+
+--- Soal nomor 10 (NOT SOLVED) ---
 noUpperAndIndent text =  filter Char.isLower text
-
-
